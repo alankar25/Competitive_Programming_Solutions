95e6dcf72eef0466e2b64c1b247d3fe466decc9b
Alankar Saxena <alankar.2573@gmail.com>
2024-09-03 00:58:59 +0530

    Cf round 969A

---
+++ A_Dora_s_Set.cpp
@@ -0,0 +1,41 @@
+//Jai Shree Ram
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define srt(v) sort(v.begin(),v.end())
+#define desort(v) sort(v.begin(),v.end(),greater<int>())
+const int MOD = 1e9+7;
+#define pb push_back
+#define FOR(i,A,n) for(int i=A;i<n;i++)
+#define FORN(i,A,n) for(int i=A;i<=n;i++)
+#define theforce ios::sync_with_stdio(false);cin.tie(0);
+#define input(x) int x;cin>>x;v.pb(x);
+#define inp(n) int n;cin>>n;
+#define endl '\n'
+using vi = vector<int>;
+// to get ith bit and with 1<<i
+void solve(){
+    int l,r; cin>>l>>r;
+    int cnt=0;
+    for(int i=l;i<=r;i++){
+        if(i%2==0) continue;
+        else if((r-i+1)>=3){
+            cnt++;
+            i+=2;
+        }
+    }
+    cout<<cnt<<endl;
+}
+signed main(){
+    theforce;
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
+/*Don't get stuck with one approach
+  Keep trying till you get it done
+  Don't lose your cool
+  All the best
+*/
