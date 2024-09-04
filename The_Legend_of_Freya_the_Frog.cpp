dc413b5d207b1eaa7b12ab23a8cabf0240f801d1
Alankar Saxena <alankar.2573@gmail.com>
2024-09-04 19:50:33 +0530

    Cf 971 Div4 C

---
+++ The_Legend_of_Freya_the_Frog.cpp
@@ -0,0 +1,42 @@
+//Jai Shree Ram
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define srt(v) sort(v.begin(),v.end())
+#define desort(v) sort(v.begin(),v.end(),greater<int>())
+const int MOD = 1e9+7;
+#define pb push_back
+#define FOR(i,A,n) for(int i=A;i<n;i++)
+#define FORN(i,A,n) for(int i=A;i<=n;i++)
+#define theforce ios::sync_with_stdio(false);cin.tie(0);
+#define input(x) int x;cin>>x;v.pb(x);
+#define inp(n) int n;cin>>n;
+#define endl '\n'
+using vi = vector<int>;
+// to get ith bit and with 1<<i
+void solve(){
+    int x,y,k;
+    cin>>x>>y>>k;
+    int a=0, b=0;
+    if(x%k==0) a=x/k;
+    else a=x/k+1;
+    if(y%k==0) b=y/k;
+    else b=y/k+1;
+    if(a>=b){
+        cout<<2*a-1<<endl;
+    }
+    else cout<<2*b<<endl;
+}
+signed main(){
+    theforce;
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
+/*Don't get stuck with one approach
+  Keep trying till you get it done
+  Don't lose your cool
+  All the best
+*/
